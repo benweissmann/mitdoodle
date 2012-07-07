44566a05e587801073597fec1a20183e27a74936
Ben Weissmann <benweissmann@gmail.com>
2012-07-07 12:01:00 -0700

    typo

--- config/initializers/paths.rb
+++ config/initializers/paths.rb
@@ -1,5 +1,5 @@
 # url of site without any path prefixes, e.g. http://bsw.scripts.mit.edu
-MITDOODLE_BARE_HOME = 'http://localhost:3000'
+MITDOODLE_HOME_BARE = 'http://localhost:3000'
 
 # url of site with path prefixes, e.g. http://bsw.scripts.mit.edu/mitdoodle
 MITDOODLE_HOME = 'http://localhost:3000'
