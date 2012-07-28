7901dd813ba7f38dfd7df4a69bf34cfc46c14016
Ben Weissmann <benweissmann@gmail.com>
2012-07-28 14:43:05 -0700

    Update sample paths

--- config/initializers/paths.rb
+++ config/initializers/paths.rb
@@ -1,3 +1,6 @@
-# url of site with path prefixes, e.g. http://bsw.scripts.mit.edu/mitdoodle
+# url of site with path prefixes, e.g. https://bsw.scripts.mit.edu:444/mitdoodle
 MITDOODLE_HOME = 'http://localhost:3000'
-MITDOODLE_PERMALINK_HOME = 'http://foo'
+
+# url that shortened urls should point to -- this should be something you're
+# prepared to maintain, e.g. http://mitdoodle.mit.edu
+MITDOODLE_PERMALINK_HOME = 'http://localhost:3000'
